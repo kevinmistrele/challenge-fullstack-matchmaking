c6c08ba5fb68e6c3bac7a3b01c8898e4e6559c3b
kevinmistrele <kevintavaresmistrele@gmail.com>
2026-02-16 09:29:31 -0300

    feat: add redux-saga middleware to store configuration

--- src/application/store/index.ts
+++ src/application/store/index.ts
@@ -1,10 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
+import createSagaMiddleware from 'redux-saga';
 import appReducer from './slices/app-slice';
 
+const sagaMiddleware = createSagaMiddleware();
+
+// Configure a store do Redux usando o appReducer e o middleware de saga
 export const store = configureStore({
   reducer: {
     app: appReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ thunk: false }).concat(sagaMiddleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
