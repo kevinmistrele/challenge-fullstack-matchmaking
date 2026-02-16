a9c656381a7495e66abc0c3e13f163095365f1ed
kevinmistrele <kevintavaresmistrele@gmail.com>
2026-02-16 09:27:30 -0300

    feat: wrap children in ErrorBoundary for improved error handling

--- src/application/providers/app-provider.tsx
+++ src/application/providers/app-provider.tsx
@@ -3,13 +3,17 @@ import { Toaster } from '@components/ui/sonner';
 import { store } from '@application/store';
 import type { ReactNode } from 'react';
 import { QueryProvider } from './QueryProvider';
+import { ErrorBoundary } from 'react-error-boundary';
+import { ErrorFallback } from '@components/layout/route/error-fallback';
 
 export function AppProvider({ children }: { children: ReactNode }) {
   return (
     <ReduxProvider store={store}>
       <QueryProvider>
-        {children}
-        <Toaster position="top-right" richColors />
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+          {children}
+          <Toaster richColors position="top-right" />
+        </ErrorBoundary>
       </QueryProvider>
     </ReduxProvider>
   );
